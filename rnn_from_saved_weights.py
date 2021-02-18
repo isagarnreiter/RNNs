900e930a06fbd94c1c8ac09c12b75c7fc43fc480
isagarnreiter <51999763+isagarnreiter@users.noreply.github.com>
2021-02-18 12:56:56 +0000

    update

--- rnn_from_saved_weights.py
+++ rnn_from_saved_weights.py
@@ -10,18 +10,19 @@ import matplotlib.pyplot as plt
 from weights_fct import plot_weights
 from psychrnn.backend.models.basic import Basic
 import tensorflow as tf0
-from oli_task import PerceptualDiscrimination
+from oli_task_modif import PerceptualDiscrimination
 
+%matplotlib inline
 #%%
 
-weights = dict(np.load('./weights/saved_weights_1_2_output.npz', allow_pickle = True))
+#weights_1 = dict(np.load('./weights/saved_weights_1_2_output.npz', allow_pickle = True))
+weights = dict(np.load('./weights/saved_weights_new_task_sparse_connectivity_3.npz', allow_pickle = True))
+
+
 # =============================================================================
 # changes to be made to the weights
 # =============================================================================
 weights['W_in'][:,1] = 0
-
-# weights['W_rec'][40:80, 20:40] = 0
-# weights['W_rec'][90:100, 20:40] = 0
 
 np.savez('./weights/modified_saved_weights.npz', **weights)
 
@@ -33,25 +34,30 @@ N_batch = 50 # The number of trials per training update.
 N_rec = 100 # The number of recurrent units in the network.
 N_in = 3
 N_out = 2
-name = 'modif_model' #  Unique name used to determine variable scope for internal use.
+name = 'dale_model' #  Unique name used to determine variable scope for internal use.
 
 pd = PerceptualDiscrimination(dt = dt, tau = tau, T = T, N_batch = N_batch) # Initialize the task object
-modif_network_params = pd.get_task_params() # get the params passed in and defined in pd
-modif_network_params['N_rec'] = N_rec # set the number of recurrent units in the model
-modif_network_params['name'] = name
-modif_network_params['N_in'] = N_in
-modif_network_params['N_out'] = N_out
-modif_network_params['dale_ratio'] = .8
+file_network_params = pd.get_task_params() # get the params passed in and defined in pd
+file_network_params['N_rec'] = N_rec # set the number of recurrent units in the model
+file_network_params['name'] = name
+file_network_params['N_in'] = N_in
+file_network_params['N_out'] = N_out
+
+#=============================================================================
+#build array depicting connections between 2 seperate excitatory + inhibitory domains. 
+#domains receive input from either of the 2 channels
+#10% of connexions are weighted randomly between domains
 
 
-modif_network_params['load_weights_path'] = './weights/modified_saved_weights.npz'
+file_network_params['load_weights_path'] = './weights/modified_saved_weights.npz'
 
-fileModel = Basic(modif_network_params)
+fileModel = Basic(file_network_params)
 
 #%%
 weights = fileModel.get_weights()
 plot_weights(weights['W_rec'], xlabel = 'From', ylabel = 'To')
 plot_weights(weights['W_in'])
+plot_weights(weights['W_out'])
 
 #%%
 x, y,mask, train_params = pd.get_trial_batch() # get pd task inputs and outputs
@@ -60,7 +66,8 @@ model_output, model_state = fileModel.test(x) # run the model on input x
 Accuracy = pd.accuracy_function(y, model_output, mask)
 
 #%%
-trial_nb = 7
+# ---------------------- Plot the results ---------------------------
+trial_nb = 26
 for i in range(len(mask[trial_nb])):
     if mask[trial_nb][i][0] == 0:
         y[trial_nb][i] =+ np.nan
@@ -74,17 +81,20 @@ ax1.legend(["Input Channel 1", "Input Channel 2", 'go cue'])
 
 ax2 = plt.subplot(222)
 ax2.plot(range(0, len(x[0,:,:])*dt,dt), y[trial_nb,:,:])
-ax2.set_title("Target output", fontsize = 16)
+ax2.set_title("Expected output", fontsize = 16)
+ax2.set_ylim(-0.1,1.1)
 
 ax3 = plt.subplot(224)
 ax3.plot(range(0, len(x[0,:,:])*dt,dt), model_output[trial_nb,:,:])
 ax3.set_xlabel("Time (ms)", fontsize = 16)
 ax3.set_title("Output", fontsize = 16)
+ax3.set_ylim(-0.1,1.1)
 
 ax4 = plt.subplot(223)
 ax4.plot(range(0, len(x[0,:,:])*dt,dt), model_state[trial_nb,:,:])
 ax4.set_xlabel("Time (ms)", fontsize = 16)
 ax4.set_title("State of each neuron", fontsize = 16)
+ax4.set_ylim(-0.5,0.5)
 
 fig2.tight_layout()
 
@@ -95,15 +105,15 @@ ax1.plot(range(0, len(x[0,:,:])*dt,dt), model_state[trial_nb,:,80:90], c = 'blue
 ax1.plot(range(0, len(x[0,:,:])*dt,dt), model_state[trial_nb,:,0:30], c = 'red', alpha=0.6)
 ax1.plot(range(0, len(x[0,:,:])*dt,dt), model_state[trial_nb,:,30:40], c = 'black', alpha=0.6)
 ax1.set_title("State of each neuron in H1", fontsize = 10)
-ax1.set_ylim(-1, 1.5)
+ax1.set_ylim(-0.5, 0.5)
 
 ax2 = plt.subplot(212)
-ax2.plot(range(0, len(x[0,:,:])*dt,dt), model_state[trial_nb,:,40:70], c='red', alpha=0.6)
+ax2.plot(range(0, len(x[0,:,:])*dt,dt), model_state[trial_nb,:,40:70], c = 'red', alpha=0.6)
 ax2.plot(range(0, len(x[0,:,:])*dt,dt), model_state[trial_nb,:,90:100], c='blue', alpha=0.6)
 ax2.plot(range(0, len(x[0,:,:])*dt,dt), model_state[trial_nb,:,70:80], c='black', alpha=0.6)
 ax2.set_xlabel("Time (ms)", fontsize = 10)
 ax2.set_title("State of each neuron in H2", fontsize = 10)
-ax2.set_ylim(-1, 1.5)
+ax2.set_ylim(-0.5, 0.5)
 
 plt.tight_layout()
 
